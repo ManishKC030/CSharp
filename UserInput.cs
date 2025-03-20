f5f4e9d5c3ad228e3c3bb3b627cd0b7be100b1c4
Manish Kc <manishkc2004@gmail.com>
2025-03-20 19:01:20 +0545

    Taking User input in C#

---
+++ UserInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class UserInput {
+static void Main()
+    {
+        Console.WriteLine("Enter Your Namme");
+        string username = Console.ReadLine();
+        Console.WriteLine("Your name is" + " " +username );
+
+        Console.WriteLine("Enter your roll number");
+       /*  
+          // int roll = Console.ReadLine();
+        this will give you error as readline only returns a string
+         we use type casting for this 
+       */
+
+        int roll = Convert.ToInt32( Console.ReadLine() );
+
+        Console.WriteLine("YOur roll number is : " + roll);
+    }
+}
