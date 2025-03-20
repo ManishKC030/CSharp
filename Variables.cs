a16b0d23b45b93731a0d82ab18fad6f9a0750b51
Manish Kc <manishkc2004@gmail.com>
2025-03-20 17:05:21 +0545

    variables in C#

---
+++ Variables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Variables
+{
+    static void Main()
+    {
+        //variables are the container or storage location that hold data or a value
+
+        int myNum = 20;
+        myNum = 30; // this changes myNum to 30
+        Console.WriteLine(myNum);
+
+        char myChar = 'A';
+        Console.WriteLine(myChar);
+
+        string myStr = "Hello There";
+        Console.WriteLine(myStr);
+
+    }
+}
