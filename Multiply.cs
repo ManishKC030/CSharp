2811d54c26a09316fca130d27ebd4d9ff595a88d
Manish Kc <manishkc2004@gmail.com>
2025-03-20 12:40:12 +0545

    Geeting into it

---
+++ Multiply.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Multiply
+{ 
+      static void Main()
+        {
+        int a = 5;
+        int b = 5;
+        int c = a*b;
+
+        Console.WriteLine(c);
+        }
+}
