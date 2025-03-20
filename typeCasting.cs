3edf0b2bf588194beda305e2869fa7d37a71470e
Manish Kc <manishkc2004@gmail.com>
2025-03-20 18:32:10 +0545

    Type casting

--- typeCasting.cs
+++ typeCasting.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Getting_Started
-{
-    internal class typeCating
+class typeCasting { 
+
+    //type casting means  converting one data type into another
+    /*  
+     there are 2 types of type casting in c#
+        . implicit casting(automatically) -> converting smaller type to larger
+        . explicit casting(manually) ->  converting larger type to samller
+            
+     */
+    static void Main()
     {
+        int myInt = 10;
+        float myFloat = myInt; //automatic conversion
+
+        Console.WriteLine(myFloat);
+       
+    
+          double myDouble = 9.28; 
+           int myInt2 = (int)myDouble;
+        Console.WriteLine(myInt2);
+       
+        //we can laso use (Convert.To....)
+        Console.WriteLine(Convert.ToDouble(myInt));
+
     }
-}
+} 
