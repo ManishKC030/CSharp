6048796c83773423d801fa25775f9a0edda885f0
Manish Kc <manishkc2004@gmail.com>
2025-03-21 13:00:32 +0545

    Ternary Operator in C#

---
+++ If_Else.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class If_Else {
+
+    static void Main()
+    {
+        // if , else, and else if is same as in C
+
+        //  short hand if..else(ternary operator)
+        // synatx: variablle = (condition) ? experssionTrue : experssionFalse
+
+        var x = 7;
+        var y = 9;
+        if (x < y)
+        {
+            Console.WriteLine($" y ({y}) is greater than x ({x})");
+        }
+        else
+        {
+            Console.WriteLine($" x ({x}) is greater than y ({y})");
+
+        }
+
+        //we can use short hand if..else in this way
+        string result = (x < y) ? "Y greater" : "X greater";
+        Console.WriteLine(result);
+        
+    }
+}
+
+
