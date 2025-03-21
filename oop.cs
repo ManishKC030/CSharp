950dbdd4b68165be1ad9c160e5abf5fab1843c05
Manish Kc <manishkc2004@gmail.com>
2025-03-21 19:30:12 +0545

    Object oriented programming

---
+++ oop.cs
@@ -0,0 +1,26 @@
+﻿//class serves as a blueprint or template for creating an object
+//object is an instance of class which repersents real world entity
+//example: car is a class then nissan, toyota, honda are the object 
+
+
+using System;
+
+
+class Car
+{
+    public string color = "red";
+    public string model = "Toyota";
+
+}
+
+class oops {
+
+    static void Main()
+    {
+       Car myobj = new Car();
+        Console.WriteLine(myobj.color);
+        Console.WriteLine("Your car model is" + myobj.model);
+    }
+
+
+}
