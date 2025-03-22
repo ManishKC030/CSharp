4ab98d777b37dd4d36395219f3cb0ef222519e6b
Manish Kc <manishkc2004@gmail.com>
2025-03-22 14:09:48 +0545

    Polymorphism in C#

---
+++ Polymorphism.cs
@@ -0,0 +1,40 @@
+﻿// Polymorphism means "many forms"
+
+using System;
+
+class Animal
+{
+ public virtual void animalSound() //virtual allows parent class method to overriden
+    {
+        Console.WriteLine("Animal Makes a Sound");
+     }
+}
+
+class Dog:Animal
+{
+    public override void animalSound() //without overriding base class is only accessed
+    {
+        Console.WriteLine("Dogs barks: Bow Bow");
+    }
+}
+
+class Cat:Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("Cats Meow : Meow Meow");
+    }
+}
+
+class Polymorphism
+{
+    static void Main()
+    {
+        Animal myanimal = new Animal();
+        Animal mydog = new Dog();
+        Animal mycat = new Cat();
+         myanimal.animalSound();
+        mydog.animalSound();
+        mycat.animalSound();
+    }
+}
