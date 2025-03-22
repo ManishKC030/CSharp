fbef79c1344249eabad2bb79f7e6e4f94fe8b1ba
Manish Kc <manishkc2004@gmail.com>
2025-03-22 09:17:49 +0545

    Constructor

---
+++ constructor.cs
@@ -0,0 +1,27 @@
+﻿//constructor is special method that is automtically called when object of class is created
+
+using System;
+using System.Net.Mail;
+using System.Runtime.InteropServices;
+
+class Car
+{
+    public string brand;
+    public string model;
+    
+    //creating a constructor for Car class
+    public Car()
+    {
+        brand = "Honda";
+        model = "Civic";
+    }
+}
+
+class constructor {
+
+   static void Main()
+    {
+        Car objcar = new Car(); 
+        Console.WriteLine(objcar.brand);
+    }
+}
