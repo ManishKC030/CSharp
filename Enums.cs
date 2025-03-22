e74c59dd7d38029a98a3e31e7469905d12fe5561
Manish Kc <manishkc2004@gmail.com>
2025-03-22 20:46:12 +0545

    Enums in C#

---
+++ Enums.cs
@@ -0,0 +1,17 @@
+﻿// enums is a special types that represents a group of constants
+
+using System;
+
+class Enums
+{
+    enum Level 
+    {
+    High, Medium, Low
+    }
+
+    static void Main()
+    {
+        Level myVar = Level.High;
+        Console.WriteLine(myVar);
+    }
+}
