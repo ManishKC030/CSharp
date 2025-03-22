4da37ddfd095dfbf0bbd78ed057216c0827dc833
Manish Kc <manishkc2004@gmail.com>
2025-03-22 15:58:29 +0545

    Abstraction

---
+++ Abstraction.cs
@@ -0,0 +1,36 @@
+﻿// Abstraction is process of hiding certain details and showing only essentila information to user
+
+using System;
+
+abstract class Animal
+{
+    public abstract void MakeSound(); //abstract method
+
+}
+
+class Dog : Animal //inheriting dog from animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Dog says Woof Woof");
+    }
+}
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Cat says Meow Meow");
+
+    }
+
+    class Abstraction
+    {
+        static void Main()
+        {
+            Animal myDog = new Dog();
+            Animal myCat = new Cat();
+            myCat.MakeSound();
+            myDog.MakeSound();
+        }
+    }
+}
