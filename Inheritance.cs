7d2385bfaf1ac716d88dc25d771615b121785098
Manish Kc <manishkc2004@gmail.com>
2025-03-22 11:42:53 +0545

    Inheritance

---
+++ Inheritance.cs
@@ -0,0 +1,30 @@
+﻿// It is a key concept of oop that allows one class to inherit properties of another class
+
+using System;
+
+class Vehicle // parent class
+{
+    public string brand;
+    public string honk()
+    {
+        return "Tuut, Tuut!";
+    }
+}
+
+class Car : Vehicle //child class, extends Vehicle
+{
+    public string name;
+
+}
+
+class Inheritance
+{
+    static void Main()
+    {
+        Car mycar = new Car();
+        mycar.brand = "Ford";
+        mycar.name = "Mustang";
+        Console.WriteLine(mycar.name + " " + mycar.brand + " makes sound " + mycar.honk());
+        
+    }
+}
