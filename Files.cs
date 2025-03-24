a33a2f13ce1c3c77e6d878c45294da8e0ffe82e2
Manish Kc <manishkc2004@gmail.com>
2025-03-24 10:59:56 +0545

    Files in C#

---
+++ Files.cs
@@ -0,0 +1,18 @@
+﻿// we use System.IO namespace, which allows us to work with files
+// File class has many useful methods
+
+using System;
+using System.IO;
+
+
+class File_Handling
+{
+    static void Main()
+    {
+        string text = "Hello World!";
+        File.WriteAllText("filename.txt", text); //creates and writes contents of text
+
+        string readText = File.ReadAllText("filename.txt");
+        Console.WriteLine(readText);
+    }
+}
