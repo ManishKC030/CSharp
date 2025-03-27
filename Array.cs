148b7b52a14ba5dbcd6e4a13c660f7ae95cd0095
Manish Kc <manishkc2004@gmail.com>
2025-03-27 18:46:56 +0545

    Array in C#

--- Array.cs
+++ Array.cs
@@ -4,6 +4,20 @@ class Array
 {
     static void Main()
     {
-        Console.WriteLine("Hello World");
+        //to declare an array, define variable type with square brackets
+        string[] cars = { "honda", "BMW", "Ford", "Mazda" };
+        Console.WriteLine(cars[1]); //output BMW
+
+
+        // Initialization only
+        int[] numbers = new int[5];  // Array with 5 elements
+
+        // Declaration 
+         numbers = new int[] { 10, 20, 30, 40, 50 };
+
+        //in short we can use just like cars array
+        // int[] numbers = {10,20,30,40,50};
+
+        Console.WriteLine(numbers[3]);
     }
 }
