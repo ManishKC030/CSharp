126e3fe483361ebf370c26198deec0e76c1d4d6b
Manish Kc <manishkc2004@gmail.com>
2025-03-27 19:25:24 +0545

    Array Types in C#

--- ArrayTypes.cs
+++ ArrayTypes.cs
@@ -1,6 +1,6 @@
 ﻿// In C#, there are 3 main types of array
 // 1. Single-Dimensional Array
-// 2. Multi-Demensional Array
+// 2. Multi-Dimensional Array
 // 3. Jagged Array (Array of Array)
 
 using System;
@@ -8,8 +8,15 @@ class ArrayTypes
 {
     static void Main()
     {
-        //simplex array
+        //single dimensional array
         int[] number = { 1, 2, 3 };
         Console.WriteLine(number[0]); //output: 1
+
+        //multi dimensional Array uses multiple indexes
+        int[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        Console.WriteLine(matrix[1, 2]); // Output: 6
+        Console.WriteLine(matrix[0,1]); //output is 2
+
+
     }
 }
